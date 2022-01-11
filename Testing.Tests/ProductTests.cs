ec217661104fa1f847e77d226aef5cb2a7ea21db
Gleb <glebdvp@gmail.com>
2022-01-11 08:53:03 +0700

    Добавлены простые модульные тесты xUnit

---
+++ Testing.Tests/ProductTests.cs
@@ -0,0 +1,32 @@
+using Testing.Models;
+using Xunit;
+
+namespace Testing.Tests;
+
+public class ProductTests
+{
+    [Fact]
+    public void CanChangeProductName()
+    {
+        // Arrange
+        var p = new Product { Name = "Test", Price = 100M };
+
+        // Act
+        p.Name = "New Name";
+
+        // Assert
+        Assert.Equal("New Name", p.Name);
+    }
+    [Fact]
+    public void CanChangeProductPrice()
+    {
+        // Arrange
+        var p = new Product { Name = "Test", Price = 100M };
+
+        // Act
+        p.Price = 200M;
+
+        // Assert
+        Assert.Equal(100M, p.Price);
+    }
+}
